503f35afc4f85a2e8def942e3021cbdf78c60a32
Rajat Khatri <khatrirajat888@gmail.com>
2023-06-29 13:47:16 +0530

    added typewriter effect in home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,43 @@
+import { useEffect } from "react";
 import Socials from "../components/Socials";
 import "../styles/particle.css";
 
 const HomeScreen = () => {
+
+    useEffect(() => {
+        //typwriter effect to show text and delete then show another text
+        const texts = ["A Full Stack Web Developer", "A Programmer", "A Student"];
+        let count = 0;
+        let index = 0;
+        let currentText = "";
+        let letter = "";
+        let isDeleting = false;
+
+        const type = () => {
+            if (count === texts.length) {
+                count = 0;
+            }
+            currentText = texts[count];
+            if (isDeleting) {
+                letter = currentText.slice(0, --index);
+            } else {
+                letter = currentText.slice(0, ++index);
+            }
+            document.querySelector(".home-content p").textContent = letter == '' ? '.' : letter;
+            if (letter.length === currentText.length) {
+                isDeleting = true;
+            }
+            if (letter.length === 0) {
+                isDeleting = false;
+                count++;
+            }
+            setTimeout(type, 150);
+        }
+        type();
+
+
+
+    }, [])
 
     return (
         <section id="home">
@@ -14,7 +50,8 @@ const HomeScreen = () => {
             </div>
             <div className="home-content">
                 <h1>HEY, I'M RAJAT&nbsp;KHATRI</h1>
-                <p>A full-stack developer, Building Seamless mobile & web applications.</p>
+                <iframe style={{ pointerEvents: "none", userSelect: "none" }} src="https://giphy.com/embed/LEe5yo2E9Fi3FmuEPK" width="180" height="180" allowFullScreen></iframe>
+                <p></p>
             </div>
             <div className="home-scials">
                 <Socials />
