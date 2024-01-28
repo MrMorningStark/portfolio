d4baed912f805b294815efe96484a1b877b5c19e
Rajat Khatri <khatrirajat888@gmail.com>
2024-01-28 12:07:43 +0530

    modified completed projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <footer>
                 <div className="footer-top">
                     <h3>CONTACT ME</h3>
-                    <p>Leave a message on my any of my social media, i'll reach out to you soon.</p>
+                    <p>Feel free to contact me anytime and I will get back to you as soon as possible.</p>
                     <Socials />
                 </div>
                 <div className="footer-bottom">
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -3,6 +3,7 @@ import Card from "../components/Card";
 import ProjectReadMore from "../components/ProjectReadMore";
 import brainWaveDemoGif from "../images/BrainWaveDemo.gif";
 import brainWaveImg from "../images/BrainWave.jpg";
+import dataShuttleproImg from '../images/data_shuttle_pro.jpg';
 
 const Projects = () => {
 
@@ -10,41 +11,41 @@ const Projects = () => {
 
     const projectdData = [
         {
-            img: brainWaveImg,
-            gif: brainWaveDemoGif,
-            name: 'Brain Wave',
-            title: 'Brain Wave',
-            description: 'Brain Wave built with ReactJS, NodeJS. It uses the OpenAI API to respond to messages with a human-like response. It also has a voice feature that can read out the response. User can ask question using voice or text.',
-            liveDemo: 'https://chat-client-1oud.onrender.com/'
-        },
-        {
-            img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FMusive-3.PNG&w=128&q=75',
-            name: 'MUSIVE',
-            title: 'Fully Functional music player with UI like Spotify app.',
-            description: 'Full-stack music player app is written in flutter and dart using node.js music API. Fully Functional music player with UI like Spotify app. Added 1000+ royalty-free music and information about artists. Application users can make their own playlist or collect liked songs. ',
-            liveDemo: 'https://chat-client-1oud.onrender.com/'
-        },
-        {
-            img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FPhotoarc-1.PNG&w=128&q=75',
-            name: 'PHOTOARC',
-            title: 'A movie search app',
-            description: 'A movie search app built with React, Next.js, and Tailwind CSS. It uses the TMDB API to fetch movies and display them.',
-            liveDemo: 'https://chat-client-1oud.onrender.com/'
-        },
-        {
-            img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FMoviedb-2.PNG&w=128&q=75',
-            name: 'MOVIEDB',
-            title: 'A movie search app',
-            description: 'A movie search app built with React, Next.js, and Tailwind CSS. It uses the TMDB API to fetch movies and display them.',
-            liveDemo: 'https://chat-client-1oud.onrender.com/'
-        },
-        {
-            img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FCineast-1.PNG&w=128&q=75',
-            name: 'Cineast',
-            title: 'A movie search app',
-            description: 'A movie search app built with React, Next.js, and Tailwind CSS. It uses the TMDB API to fetch movies and display them.',
+            img: dataShuttleproImg,
+            name: 'Data Shuttle Pro',
+            title: 'File Transfer',
+            description: 'Effortlessly move your files across the digital universe with Data Shuttle Pro! Seamlessly transfer data between FTP, Google Drive, SFTP, and local storage. Your data, your way!',
             liveDemo: ''
         },
+        // {
+        //     img: brainWaveImg,
+        //     gif: brainWaveDemoGif,
+        //     name: 'Brain Wave',
+        //     title: 'Brain Wave',
+        //     description: 'Brain Wave built with ReactJS, NodeJS. It uses the OpenAI API to respond to messages with a human-like response. It also has a voice feature that can read out the response. User can ask question using voice or text.',
+        //     liveDemo: 'https://chat-client-1oud.onrender.com/'
+        // },
+        // {
+        //     img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FMusive-3.PNG&w=128&q=75',
+        //     name: 'MUSIVE',
+        //     title: 'Fully Functional music player with UI like Spotify app.',
+        //     description: 'Full-stack music player app is written in flutter and dart using node.js music API. Fully Functional music player with UI like Spotify app. Added 1000+ royalty-free music and information about artists. Application users can make their own playlist or collect liked songs. ',
+        //     liveDemo: 'https://chat-client-1oud.onrender.com/'
+        // },
+        // {
+        //     img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FPhotoarc-1.PNG&w=128&q=75',
+        //     name: 'PHOTOARC',
+        //     title: 'A movie search app',
+        //     description: 'A movie search app built with React, Next.js, and Tailwind CSS. It uses the TMDB API to fetch movies and display them.',
+        //     liveDemo: 'https://chat-client-1oud.onrender.com/'
+        // },
+        // {
+        //     img: 'https://anshrathod.com/_next/image?url=%2Fimgs%2FMoviedb-2.PNG&w=128&q=75',
+        //     name: 'MOVIEDB',
+        //     title: 'A movie search app',
+        //     description: 'A movie search app built with React, Next.js, and Tailwind CSS. It uses the TMDB API to fetch movies and display them.',
+        //     liveDemo: 'https://chat-client-1oud.onrender.com/'
+        // },
     ];
 
     const handleReadMore = (data) => {
